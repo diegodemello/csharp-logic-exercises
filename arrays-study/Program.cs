84ffa6775ebb43e54c5ded18a3723a5acabaf20f
Diego <diegodemello027@gmail.com>
2024-11-29 19:27:19 -0300

    Leia uma matriz quadrada, calcule a soma de todos os elementos positivos, leia um índice de coluna e imprima, leia um índice de linha e imprima, imprima a diagonal principal e eleve ao quadrado os números negativos e imprima a matriz inteira alterada.

--- arrays-study/Program.cs
+++ arrays-study/Program.cs
@@ -4,38 +4,76 @@
     {
         static void Main(string[] args)
         {
-            string[] op = Console.ReadLine().Split(" ");
-            int m = int.Parse(op[0]);
-            int n = int.Parse(op[1]);
+            int n = int.Parse(Console.ReadLine());
+            int[,] mz = new int[n, n];
 
-            int[,] pmMz = new int[m, n];
-            int[,] scMz = new int[m, n];
-            int[,] fnMz = new int[m, n];
-
-            for (int i = 0; i < m; i++)
+            for (int i = 0; i < n; i++)
             {
                 string[] s = Console.ReadLine().Split(" ");
                 for (int j = 0; j < n; j++)
                 {
-                    pmMz[i,j] = int.Parse(s[j]);
+                    mz[i,j] = int.Parse(s[j]);
                 }
             }
 
-            for (int i = 0; i < m; i++)
+            int soma = 0;
+            for (int i = 0; i < n; i++)
             {
-                string[] s = Console.ReadLine().Split(" ");
                 for (int j = 0; j < n; j++)
                 {
-                    scMz[i, j] = int.Parse(s[j]);
+                    if (mz[i, j] > 0)
+                    {
+                        soma = soma + mz[i, j];
+                    }
                 }
             }
+            Console.WriteLine("Soma dos números positivos: " + soma);
 
-            for (int i = 0; i < m; i++)
+            int indiceLinha = int.Parse(Console.ReadLine());
+            Console.Write("Linha Escolhida: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mz[indiceLinha, i] + " ");
+            }
+            Console.WriteLine();
+
+            int indiceColuna = int.Parse(Console.ReadLine());
+            Console.Write("Coluna Escolhida: ");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(mz[j, indiceColuna] + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Diagonal Principal: ");
+            for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    fnMz[i, j] = pmMz[i, j] + scMz[i, j];
-                    Console.Write(fnMz[i, j] + " ");
+                    if(i == j)
+                    {
+                        Console.Write(mz[i, j] + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mz[i,j] < 0)
+                    {
+                        mz[i,j] = mz[i,j]*mz[i,j];
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0;j < n; j++)
+                {
+                    Console.Write(mz[i, j] + " ");
                 }
                 Console.WriteLine();
             }
